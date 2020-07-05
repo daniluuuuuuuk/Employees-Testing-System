a35f1c93477df497ae2da7ccc5ae0354713268e6
daniluuuuuuuk <artyom.danilyuk.420@mail.ru>
2020-07-05 03:18:05 +0300

    class Account added

--- src/account/models.py
+++ src/account/models.py
@@ -3,4 +3,27 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 
 class Account(AbstractBaseUser):
-    pass
+    email = models.EmailField(verbose_name="email", max_length=60, unique=True)
+    username = models.CharField(max_length=30, unique=True)
+    first_name = models.CharField(max_length=30, unique=False)
+    last_name = models.CharField(max_length=30, unique=False)
+    patronage = models.CharField(max_length=30, unique=False)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['username']
+
+    objects = MyAccountManager()
+
+    def __str__(self):
+        return self.username
+
+# For checking permissions. to keep it simple all admin have ALL permissons
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    # Does this user have permission to view this app? (ALWAYS YES FOR SIMPLICITY)
+    def has_module_perms(self, app_label):
+        return True
